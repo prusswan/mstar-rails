9f255c845232a5dc217ab10e84d308197290190b
prusswan <prusswan@gmail.com>
2012-12-21 01:53:07 +0800

    initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MstarRails::Application.config.secret_token = 'b84de6708c4923e05b321f53d4a4f630f67aa2fbc38ed7e35d03599ac964f9664eaf9048706799851fa3e8e6dd95ec5609bd17679bd74853010521a6aaf4c9d6'
