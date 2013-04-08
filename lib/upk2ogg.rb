89b121668dbd2737959c40f215b9bb5c0dfdd28d
prusswan <prusswan@gmail.com>
2013-04-09 05:36:56 +0800

    updated upk2ogg to v2

--- lib/upk2ogg.rb
+++ lib/upk2ogg.rb
@@ -1,36 +1,44 @@
 dir = 'F:\Program Files (x86)\GarenaMStarTW\Apps\MStarTW\NurienGame\Content\Music'
-outdir = 'F:\Mstar\v1'
+# dir = 'C:\Program Files (x86)\GarenaMStarTW\Apps\MStarTW\NurienGame\Content\Music'
+# dir = 'C:\Users\prusswan\Desktop\Mstar'
+# outdir = 'C:\Users\prusswan\Desktop\Ogg'
+outdir = 'E:\Mstar\ogg'
 
 Dir.foreach(dir) do |filename|
 
-  # next unless filename =~ /^m.+\.upk$/
-  next unless filename == 'm_a3k_0197.upk' or filename == 'm_a1f_0001.upk'
+  next unless filename =~ /^m_a.+\.upk$/
+  # next unless filename == 'm_a3k_0197.upk' or filename == 'm_a1f_0001.upk'
 
-  f = File.new("#{dir}\\#{filename}")
-  out = File.new("#{outdir}\\#{filename.gsub('upk','ogg')}", 'wb')
+  out_file = "#{outdir}\\#{filename.gsub('upk','ogg')}"
+  next if File.exist?(out_file)
+
+  f = File.open("#{dir}\\#{filename}", 'rb')
+  out = File.new(out_file, 'wb')
+
+  offset1 = 560 # 540
+  offset2 = offset1 + 38 # 598
+  mask = 240
 
   count = 0
   f.each_byte do |b|
-    count += 1
-    
-    if count < 560
-    
-    elsif count < 598
-      if count%4 < 2
-        # puts "%02X: #{count}" % (b ^ 240)
-        out.write (b^240).chr
-      else
-        # puts "%02X: #{count}" % b
-        out.write b.chr
+    # count += 1
+    char = b
+
+    next if count < offset1
+
+    if count < offset2
+      if (count - offset1)%4 < 2
+        char = (b ^ mask)
       end
-    else
-      out.write b.chr
+      # puts "%02X: #{count}" % char
     end
+
+    out.write char.chr
   end
 
   f.close
   out.close
-  
-  puts "#{filename} processed"
+
+  puts "#{filename} processed (#{count} bytes)"
 
 end
