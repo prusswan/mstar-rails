8adda01a67f307256bdcb375a530b8f274cdcd42
prusswan <prusswan@gmail.com>
2013-05-16 17:28:24 +0800

    fixed regex warning, much fun to be had with 60+ test failures

--- app/models/move.rb
+++ app/models/move.rb
@@ -2,8 +2,8 @@ class Move < ActiveRecord::Base
   attr_accessible :mstar_id, :sequence
 
   validates :mstar_id, presence: true, numericality: true, uniqueness: true
-  validates :sequence, presence: true, format: /^([\d\(\)]|(\[[\(\)]\d\]))+$/
+  validates :sequence, presence: true, format: /\A([\d\(\)]|(\[[\(\)]\d\]))+\z/
 
   # regex to check that parens are closed and not nested
-  validates :sequence, format: /^[^\(\)]*(\([^\(\)]*\))*[^\(\)]*$/
+  validates :sequence, format: /\A[^\(\)]*(\([^\(\)]*\))*[^\(\)]*\z/
 end
